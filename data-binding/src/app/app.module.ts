fd209991587279b9bbb4b918dd8d74e4a8342196
allan-mlpe <allan_mlpe@hotmail.com>
2017-06-08 16:24:24 -0300

    Utilizando class e style binding

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { StyleBindComponent } from './style-bind/style-bind.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    StyleBindComponent
   ],
   imports: [
     BrowserModule
