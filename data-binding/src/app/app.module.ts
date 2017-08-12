e61d9c3742fa01d360d9ad4016ce19419d9fd636
allan-mlpe <allan_mlpe@hotmail.com>
2017-08-12 16:30:37 -0300

    Importando o módulo de formulários FormsModule do @angular/forms

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { StyleBindComponent } from './style-bind/style-bind.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
     StyleBindComponent,
-    EventBindingComponent
+    EventBindingComponent,
+    TwoWayBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule //para usar o ngModel em formulários é necessário importar este módulo
   ],
   providers: [],
   bootstrap: [AppComponent]
