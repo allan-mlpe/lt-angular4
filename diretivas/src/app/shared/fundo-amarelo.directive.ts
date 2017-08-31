148a3d901e0c8ad2185e805c93a0dc8d78e13654
allan-mlpe <allan_mlpe@hotmail.com>
2017-08-31 20:38:28 -0300

    Corrigindo problemas de segurança utilizando a classe Renderer

--- diretivas/src/app/shared/fundo-amarelo.directive.ts
+++ diretivas/src/app/shared/fundo-amarelo.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
-  selector: '[fundoAmarelo]'
+  selector: 'p[fundoAmarelo]' //estamos definindo que essa diretiva seja aplicável apenas a elementos do tipo p
+                              //também é possível definir que as diretivas sejam aplicadas apenas a outros componentes
+                              //basta usar o seletor do componente!
 })
 export class FundoAmareloDirective {
 
-  constructor() { }
+  constructor(private _elementRef: ElementRef, private _renderer: Renderer) { 
+    //o código abaixo é passível de ataques devido a problemas de segurança
+    //this._elementRef.nativeElement.style.backgroundColor = "yellow";
 
+    //corrigindo o problema de segurança com o renderer.
+    this._renderer.setElementStyle(
+      this._elementRef.nativeElement, //passamos o nativeElement do ElementRef
+      "background-color", //definimos o nome da propriedade igual ao CSS
+      "yellow" //valor da propriedade
+    );
+  }
 }
