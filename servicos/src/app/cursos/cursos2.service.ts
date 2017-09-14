62a67b5bcf9bfbc1732e9fa478b025c7bb48c1ab
allan-mlpe <allan_mlpe@hotmail.com>
2017-09-13 22:44:49 -0300

    Inclusão de atributos para realização de broadcast de eventos com o EventEmitter

--- servicos/src/app/cursos/cursos2.service.ts
+++ servicos/src/app/cursos/cursos2.service.ts
@@ -1,15 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 
 //declarando que a classe é injetável
 @Injectable()
 export class Cursos2Service {
+
+    /*
+        O atributo emitirCursoCriado estará disponível
+        para todos os componentes que compartilhem a
+        mesma instância do serviço
+    */
+    emitirCursoCriado = new EventEmitter<string>(); //podemos ou não tipar o EventEmitter
+
+    /*
+        Com o atributo estático a emitirCursoCriado
+        será compartilhada por todas as instâncias do serviço
+        mesmo que sejam instâncias diferentes
+    */
+    static emitirCursoCriadoStatic = new EventEmitter<string>();
 
     cursos: string[] = ['Angular', 'JavaScript', 'TypeScript'];
 
     constructor() {
         console.log('Init CursosService..');
     }
-
 
     /**
      * Obtém a lista de todos os cursos
@@ -24,5 +37,9 @@ export class Cursos2Service {
      */
     addCurso(curso: string) {
         this.cursos.push(curso);
+
+        this.emitirCursoCriado.emit(curso);
+
+        Cursos2Service.emitirCursoCriadoStatic.emit(curso);
     }
 }
