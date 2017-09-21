f6caa7fce79792eba235eb5b77e35806fab87e7e
allan-mlpe <allan_mlpe@hotmail.com>
2017-09-20 23:40:51 -0300

    Adicionando o Locale para internacionalização da aplicação

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,21 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
-    CamelCasePipe, //assim como os módulos, os pipes devem vir no metadado declarations
+    CamelCasePipe //assim como os módulos, os pipes devem vir no metadado declarations
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    /*{
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    }*/
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService) => settingsService.getLocale()
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
