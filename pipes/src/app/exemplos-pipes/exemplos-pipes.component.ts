a47886c635d554b57650731f6ebceaa910cb36f8
allan-mlpe <allan_mlpe@hotmail.com>
2017-09-21 21:55:42 -0300

    Inclusão de conteúdo da aula 48, sobre a Utilização do pipe async

--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Rx";
 
 @Component({
   selector: 'app-exemplos-pipes',
@@ -19,6 +20,14 @@ export class ExemplosPipesComponent implements OnInit {
   listaCursos: string[] = ['Angular', 'Java'];
 
   filtro: string = '';
+
+  //valor passado no resolve será atribuído à variável valorAsync após 2 segundos
+  valorAsync = new Promise((resolve, reject) =>
+    setTimeout(() => resolve('Valor assíncrono'), 2000)
+  );
+
+  //o valor será atribuído à variável valorAsync2 após 3 segundos
+  valorAsync2 = Observable.interval(3000).map( v => 'Valor assíncrono com Observable');
 
   constructor() { }
 
