e9a848cf5821b84ce2ab1d5e3e56cd12685e2a1a
allan-mlpe <allan_mlpe@hotmail.com>
2017-09-24 21:28:14 -0300

    Configuração inicial do arquivo de rotas da aplicação

---
+++ rotas/src/app/app.routing.ts
@@ -0,0 +1,27 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+
+/*
+    O objeto do tipo Routes é um array que recebe objetos
+    contendo chave-valor com duas propriedades básicas:
+
+    path: o caminho que será usado no browser
+    component: o componente que será renderizado no path indicado
+*/
+const APP_ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'cursos', component: CursosComponent},
+    { path: 'login', component: LoginComponent }
+];
+
+/*
+    RouterModule.forRoot: para arquivos que contém as rotas raízes da aplicação
+
+    RouterModule.forChild: para rotas secundárias ou rotas de funcionalidades da aplicação.
+*/
+// é necessário fazer o export da constante com as configurações de roteamento
+export const ROUTING: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
