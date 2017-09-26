370c69218d9e804d3b1a10fc728ec6371605a25d
allan-mlpe <allan_mlpe@hotmail.com>
2017-09-26 11:08:52 -0300

    Passando e obtendo queryParams nas rotas

--- rotas/src/app/cursos/cursos.component.ts
+++ rotas/src/app/cursos/cursos.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Subscription } from 'rxjs/Rx';
 
 import { CursosService } from './cursos.service';
+
 
 @Component({
   selector: 'app-cursos',
@@ -9,14 +13,28 @@ import { CursosService } from './cursos.service';
 })
 export class CursosComponent implements OnInit {
 
+  pagina: number;
   cursos: any[];
 
-  constructor(private cursosService: CursosService) {
+  inscricao: Subscription;
+
+  constructor(private cursosService: CursosService, private route: ActivatedRoute) {
 
    }
 
   ngOnInit() {
     this.cursos = this.cursosService.getCursos();
+
+    // agora estamos pegando os queryParams
+    this.inscricao = this.route.queryParams.subscribe(
+      queryParams =>
+        this.pagina = queryParams['pagina']
+    );
+  }
+
+  ngOnDestroy() {
+    //cancelando inscrição
+    this.inscricao.unsubscribe();
   }
 
 }
