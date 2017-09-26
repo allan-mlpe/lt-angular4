3f7f20c300776756cb0a82ae42e24fe84aa0444f
allan-mlpe <allan_mlpe@hotmail.com>
2017-09-26 09:50:22 -0300

    Inclusão de conteúdo da aula 54, sobre escutar mudanças nos parâmetros das rotas

--- rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -9,16 +10,33 @@ import { ActivatedRoute } from '@angular/router';
 export class CursoDetalheComponent implements OnInit {
 
   id: string;
+  inscricao: Subscription;
 
   constructor(private route: ActivatedRoute) {
     console.log(route);
 
     //pegando o id que é passado na rota
     //o nome 'id' foi definido na configuração da rota no app.routing.ts
-    this.id = this.route.snapshot.params['id'];
+    //this.id = this.route.snapshot.params['id'];
    }
 
   ngOnInit() {
+    // realizamos uma inscrição no objeto params da 
+    // ActivatedRoute para ouvir mudanças quando elas ocorrerem
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        // atribuindo o valor do parâmetro 'id' do obj params 
+        this.id = params['id'];
+      }
+    );
+
+  }
+
+  ngOnDestroy() {
+    //cancelamos a inscrição ao destruir o componente
+    //uma vez que a mesma fica ativa por default mesmo
+    //após o componente ser destruído
+    this.inscricao.unsubscribe();
   }
 
 }
