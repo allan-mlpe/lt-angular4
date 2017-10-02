2e665b7ca9ab226a80bbe3e54b89ff05e0f234cc
allan-mlpe <allan_mlpe@hotmail.com>
2017-10-02 20:14:04 -0300

    Inclusão de conteúdo da aula 60, sobre a criação de rotas filhas

---
+++ rotas/src/app/alunos/alunos.routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+
+import { Routes, RouterModule } from '@angular/router';
+
+import { AlunosComponent } from './alunos.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+import { AlunoFormComponent } from './aluno-form/aluno-form.component';
+
+/*
+    Declaramos primeiro alunos/novo para evitar 
+    conflito com a rota alunos/:id.
+    
+    Devemos declarar sempre as rotas hardcoded primeiro.
+*/
+const alunosRoutes: Routes = [
+    { path: "alunos", component: AlunosComponent, children: [
+        { path: "novo", component: AlunoFormComponent },
+        { path: ":id", component: AlunoDetalheComponent },
+        { path: ":id/editar", component: AlunoFormComponent }
+    ]}
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(alunosRoutes)],
+    exports: [RouterModule]
+})
+export class AlunosRoutingModule {}
--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { CursosModule } from './cursos/cursos.module';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +8,8 @@ import { HomeComponent } from './home/home.component';
 
 //modulo de rotas
 import { AppRoutingModule } from './app.routing.module';
+import { CursosModule } from './cursos/cursos.module';
+import { AlunosModule } from './alunos/alunos.module';
 
 @NgModule({
   declarations: [
@@ -20,6 +20,7 @@ import { AppRoutingModule } from './app.routing.module';
   imports: [
     BrowserModule,
     CursosModule,
+    AlunosModule,
     AppRoutingModule
     //ROUTING //import da constante de rotas da aplicação
   ],
