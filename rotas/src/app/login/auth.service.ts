abfeaf70d7d1a101518694161f9eb748bcc3c291
allan-mlpe <allan_mlpe@hotmail.com>
2017-10-07 17:17:19 -0300

    Usando o serviço de autenticação para emitir informação se usuário está logado

---
+++ rotas/src/app/login/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from './usuario';
+
+@Injectable()
+export class AuthService {
+
+  private usuarioAutenticado: boolean = false;
+  mostrarMenu = new EventEmitter<boolean>();
+
+  fazerLogin(usuario: Usuario) {
+    if(usuario.nome === 'admin' && usuario.senha === '123456') {
+      this.usuarioAutenticado = true;
+
+      this.router.navigate(['/']);
+    } else {
+      this.usuarioAutenticado = false;
+    }
+    this.mostrarMenu.emit(this.usuarioAutenticado);
+  }
+
+  constructor(private router: Router) { }
+
+}
