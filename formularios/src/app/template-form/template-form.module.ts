5dc26fe55d6849488160ab60c0192b84bf9f06cd
allan-mlpe <allan_mlpe@hotmail.com>
2017-11-01 20:17:33 -0300

    Refatoração com criação de módulo TemplateFormModule

---
+++ formularios/src/app/template-form/template-form.module.ts
@@ -0,0 +1,19 @@
+import { FormDebugComponent } from './../form-debug/form-debug.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { TemplateFormComponent } from './template-form.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
+    declarations: [
+        TemplateFormComponent,
+        FormDebugComponent
+    ],
+    providers: []
+})
+export class TemplateFormModule {}
