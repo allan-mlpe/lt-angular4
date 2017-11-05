140683b7e890617e1c47b8390047a9cb7df151ac
allan-mlpe <allan_mlpe@hotmail.com>
2017-11-04 21:16:49 -0300

    Inclusão de import do HttpModule

--- formularios/src/app/template-form/template-form.module.ts
+++ formularios/src/app/template-form/template-form.module.ts
@@ -2,6 +2,7 @@ import { FormDebugComponent } from './../form-debug/form-debug.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { TemplateFormComponent } from './template-form.component';
 import { CampoControlErrorComponent } from '../campo-control-error/campo-control-error.component';
@@ -9,7 +10,8 @@ import { CampoControlErrorComponent } from '../campo-control-error/campo-control
 @NgModule({
     imports: [
         CommonModule,
-        FormsModule
+        FormsModule,
+        HttpModule //import do módulo Http
     ],
     declarations: [
         TemplateFormComponent,
