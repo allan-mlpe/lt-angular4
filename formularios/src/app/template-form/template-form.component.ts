e8f251eef69c05eb62cb7370f3919ee109c39331
allan-mlpe <allan_mlpe@hotmail.com>
2017-11-05 21:15:20 -0300

    Inclusão de conteúdo da aula 86, populando campos do formulário

--- formularios/src/app/template-form/template-form.component.ts
+++ formularios/src/app/template-form/template-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import { NgModel, NgForm } from '@angular/forms';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map'; //import do operador map
 
@@ -53,9 +53,8 @@ export class TemplateFormComponent implements OnInit {
     };
   }
 
-  buscarCEP(cep: string) {
-    console.log(cep);
-
+  buscarCEP(cep: string, form: NgForm) {
+    
     //remove caracteres não numéricos do CEP
     cep = cep.replace(/\D/g, '');
     
@@ -70,8 +69,47 @@ export class TemplateFormComponent implements OnInit {
 
         this.http.get(`//viacep.com.br/ws/${cep}/json`)
         .map(dados => dados.json())
-        .subscribe(dados => console.log(dados));
+        .subscribe(dados => {
+          //console.log(dados);
+          //console.log(form);
+          this.populaDadosForm(dados, form);
+        });
       }
     }
+  }
+
+  /**
+   * Popula os campos do formulário com os dados vindos do webservice de CEP
+   * @param dados dados vindos do webservice
+   * @param formulario formulário que será populado com os dados
+   */
+  populaDadosForm(dados: object, formulario: NgForm) {
+    //utilizando setValue precisamos passar um objeto com todos os campos do form
+    /* formulario.setValue({
+      nome: formulario.value.nome,
+      email: formulario.value.email,
+      endereco: {
+        cep: dados['cep'],
+        numero: "",
+        complemento: dados['complemento'],
+        rua: dados['logradouro'],
+        bairro: dados['bairro'],
+        cidade: dados['localidade'],
+        estado: dados['uf']
+      }
+    }); */
+
+    //utilizando patchValue podemos passar apenas os campos que queremos popular
+    formulario.form.patchValue({
+      endereco: {
+        cep: dados['cep'],
+        numero: "",
+        complemento: dados['complemento'],
+        rua: dados['logradouro'],
+        bairro: dados['bairro'],
+        cidade: dados['localidade'],
+        estado: dados['uf']
+      }
+    });
   }
 }
