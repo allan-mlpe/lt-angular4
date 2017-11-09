992368c6e4688f0a1f4f878218377a1e77d425f3
allan-mlpe <allan_mlpe@hotmail.com>
2017-11-08 22:20:54 -0300

    Inclusão de conteúdo da aula 92, sobre a submissão de dados em forms data driven

--- formularios/src/app/data-form/data-form.component.ts
+++ formularios/src/app/data-form/data-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-data-form',
@@ -13,7 +16,8 @@ export class DataFormComponent implements OnInit {
    */
   formulario: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+              private http: Http) { }
 
   ngOnInit() {
     // criação de formulário com FormGroup
@@ -35,5 +39,14 @@ export class DataFormComponent implements OnInit {
       nome: [null],
       email: [null]
     });
+  }
+
+  onSubmit() {
+    console.log(this.formulario);
+    let formData = this.formulario.value;
+
+    this.http.post('https://httpbin.org/post', JSON.stringify(formData))
+      .map(res => res)
+      .subscribe(res => console.log(res));
   }
 }
