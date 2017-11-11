c59fa35dd8269387c0a720c81cbef3de55adab05
allan-mlpe <allan_mlpe@hotmail.com>
2017-11-11 08:11:15 -0300

    Inclusão de validação de campos com a classe Validators do Angular

--- formularios/src/app/data-form/data-form.component.ts
+++ formularios/src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { Http } from '@angular/http';
 
 import 'rxjs/add/operator/map';
@@ -36,8 +36,8 @@ export class DataFormComponent implements OnInit {
     */
     // criação de formulário utilizando o FormBuilder
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null]
+      nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+      email: [null, [Validators.required, Validators.email]]
     });
   }
 
